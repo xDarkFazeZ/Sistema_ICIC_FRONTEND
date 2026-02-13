ae8f335f9fb840067cd9ae5f0e9bfad0aa0e752b
Iram <juaniramgamezaguilar@gmail.com>
2026-02-13 16:46:39 -0600

    feat: add authentication context and hooks for user management

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,24 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import ProtectedRoute from "./components/protectedRoute";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/login",
     element: <Login />,
+  },
+  {
+    element: <ProtectedRoute />, // 👈 wrapper
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <Navigate to="/dashboard" />,
   },
 ]);
